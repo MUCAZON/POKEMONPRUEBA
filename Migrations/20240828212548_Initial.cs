ff042b5ca6b3f4f2ec937e1cf5ac0eaa983e837d
Roy Sifuentes <roysifuentes15@gmail.com>
2024-08-28 17:16:18 -0500

    AGREGANDO PROYECTO

---
+++ Migrations/20240828212548_Initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POKEMONPRUEBA.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    IdPokemon = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ps = table.Column<int>(type: "int", nullable: false),
+                    Atq = table.Column<int>(type: "int", nullable: false),
+                    Def = table.Column<int>(type: "int", nullable: false),
+                    AtqEs = table.Column<int>(type: "int", nullable: false),
+                    DefEs = table.Column<int>(type: "int", nullable: false),
+                    Velo = table.Column<int>(type: "int", nullable: false),
+                    Debilidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.IdPokemon);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+        }
+    }
+}
---
+++ Modelo/Pokemon.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace POKEMONPRUEBA.Modelo
+{
+    public class Pokemon
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdPokemon { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Ps { get; set; }
+        public int Atq { get; set; }
+        public int Def { get; set; }
+        public int AtqEs { get; set; }
+        public int DefEs{ get; set; }
+        public int Velo { get; set; }
+
+        public int Debilidad { get; set; }
+    }
+}
